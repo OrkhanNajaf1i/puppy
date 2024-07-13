0486bb5915cb17fe0e44295353b70acef322701f
OrkhanNajaf1i <orkhan.njfl@gamil.com>
2024-07-13 23:54:58 +0400

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/OrkhanNajaf1i/puppy
 
 go 1.22.3
+
+require github.com/OrkhanNajaf1i/go-mod-learn v0.0.0-20240713194656-6c1a9aaa107d // indirect
--- puppy/puppy.go
+++ puppy/puppy.go
@@ -1,9 +1,16 @@
 package puppy
 
+import dogwoof "github.com/OrkhanNajaf1i/go-mod-learn/dog_woof"
+
 func Bark() string {
 	return "Bark"
 }
 
 func Barks() string {
 	return "Barks Barks"
+}
+
+func BigName() {
+	dogwoof.WhenDogGrow(Bark())
+	dogwoof.WhenDogGrow(Barks())
 }
